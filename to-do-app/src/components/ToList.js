a8e358c46165ff84667e28d6e3a1b015bc3563be
Nadir <inab.nadir.solicode@gmail.com>
2022-05-19 16:47:53 +0100

    Many lines were added

--- to-do-app/src/components/ToList.js
+++ to-do-app/src/components/ToList.js
@@ -7,31 +7,17 @@ import TodoItems from './TodoItems';
 import "../style/style.css" ;
 
 function ToList() {
+    // retrieving data from local storage 
+    const addedTodo = JSON.parse(localStorage.getItem('todos'));
+    const [todos, setTodos] = useState(addedTodo);
 
-    const mylist = JSON.parse(localStorage.getItem('todos'));
-    // console.log('mylist=',mylist);
-
-    const [todos, setTodos] = useState(mylist);
-
-    // useEffect(() => {
-    //     if (todos) {
-    //         setTodos(todos);
-    //     }
-    // }, []);
-
-
-    
-
+    // sending data to local storage 
     useEffect(()=>{
         localStorage.setItem("todos", JSON.stringify(todos))
     }, [todos]) ;
 
-
-    console.log(todos) ;
- 
-
     const DeleteHandler = (e)=>{
-        console.log(e)
+        console.log(e.value) ;
     }
 
     const editHandler = ()=>{
@@ -40,32 +26,27 @@ function ToList() {
 
     const addTodo = (todo)=>{
         if(todo.task.trim().length !==0 ){
-            // console.log(todo.task.trim().length)
         }else{
-            alert("fill the form")
+            alert("Please Fill in The form ")
             return ;
         }
 
         const newTobeDone = [todo, ...todos] ;
         setTodos(newTobeDone) ;
-
-
-        // console.log(newTobeDone) ;
-        // const test = JSON.parse(localStorage.getItem("todos"))
-        // console.log(test)
     }
+
   return (
     <div id='myForm' className='text-light w-25'>
-        <h3 className='text-dark fw-bold p-3 m-3'>Today we will</h3>
+        <h1 className='text-white p-3 m-3'>Today we will</h1>
             <TodoInput onSubmit={addTodo} /> 
             {todos.map(todo =>{
                 return(
-                    <div className='bg-info border border-2 d-flex justify-content-between p-2 m-2'>
-                        <TodoItems task={todo.task} />
+                    <div  key={todo.id}  className='bg-info border border-2 d-flex justify-content-between p-2 m-2'>
+                            <TodoItems task={todo.task} />
                         <div>
-                            <MdOutlineDeleteForever onClick={DeleteHandler} />
-                            <BiEdit onClick={editHandler} />
-                        </div>
+                            <MdOutlineDeleteForever className='bg-danger p-1 border border-2' onClick={DeleteHandler} />
+                            <BiEdit className='bg-success mx-1 p-1 border border-2' onClick={editHandler}  />       
+                        </div> 
                     </div>
                 )
             })}
--- to-do-app/src/components/TodoInput.js
+++ to-do-app/src/components/TodoInput.js
@@ -1,13 +1,14 @@
 import React, {useState} from 'react'
 
 function TodoInput(props) {
-
     const [task, setTask] = useState("") ;
 
+    // adding a task and setting setTask from the input values .
     const onChangeHandler = (event)=>{
         setTask(event.target.value)
     }
 
+    // Form 
     const submitHandler = (event)=>{
         event.preventDefault() ;
 
@@ -25,13 +26,13 @@ function TodoInput(props) {
             <input 
             className='form-control p-2'
             type="text"
-            placeholder='Enter To do '
+            placeholder='Do Something '
             value={task}
             name="todo"
             onChange={onChangeHandler}
             /> 
         </div>
-        <button className='btn btn-info text-dark fw-bold m-2 w-25'>Add To Do</button>
+        <button className='btn btn-info  text-dark fw-bold m-2 w-25'>Add To Do</button>
     </form>
   )
 }
